d28e3c5f7ad9e4f177bc62d0ae0ae634b2ec9331
NAM2412 <20520643@gm.uit.edu.vn>
2023-01-15 14:12:18 +0700

    Implement Click-To-Move

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,6 @@ public class Move : MonoBehaviour
   
     [SerializeField] Transform target; 
     NavMeshAgent navMeshAgent;
-    Ray lastRay;
     void Start() 
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
