3c721cdfdca9273c81377dd7f84fa746a3735dfe
NAM2412 <20520643@gm.uit.edu.vn>
2023-01-15 16:44:15 +0700

    Tweaking basic movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -15,7 +15,7 @@ public class Move : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
