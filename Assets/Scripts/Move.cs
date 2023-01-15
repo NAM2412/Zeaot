ba9b6eeebe0f3e983b8de945cc346a82e1079556
NAM2412 <20520643@gm.uit.edu.vn>
2023-01-15 15:35:49 +0700

    Matching animation to movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -19,8 +19,25 @@ public class Move : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
         
-        
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 globalVelocity = navMeshAgent.velocity;
+        // taking x,y,z from global and converting into local
+        Vector3 localVelocity = transform.InverseTransformDirection(globalVelocity);
+        /*Why have to do that?*/
+        /*when we're creating our velocity, nav mesh velocity that stored as the globalVelocity variable
+        within this update animated method, we are grabbing the global coordinates.
+        So the global velocity and it might be X is 273 and Y is 57 and the velocity is changing at a particular
+        rate that shows us where we are within the world, within world space.
+        And that's not so useful for our animator.
+        Our animator just wants to know Is Character running forward or is not running forward?
+        */
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed",speed);
     }
 
     private void MoveToCursor()
