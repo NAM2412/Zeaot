9554234cf5c9178f8cb3801cac2334058132dcac
NAM2412 <20520643@gm.uit.edu.vn>
2023-02-01 22:47:45 +0700

    Throttling Attack

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
+using System.Collections;
+
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeIntervalBetweenEachAttacks = 1f;
         Transform target;
+        float timeSinceLastAttack = 0f;
         void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -21,9 +27,14 @@ namespace RPG.Combat
             }
         }
 
-        private void AttackBehaviour()
+        private  void AttackBehaviour()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeSinceLastAttack > timeIntervalBetweenEachAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");   
+                timeSinceLastAttack = 0f;
+            }
+              
         }
 
         private bool GetIsInRange() // is player reach the enemy in weapon's range?
