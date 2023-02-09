11f96f010e089797348e36781b4c7131b6cf65d4
NAM2412 <20520643@gm.uit.edu.vn>
2023-02-09 14:25:43 +0700

    Loopings Waypoints (UI)

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -9,10 +9,26 @@ namespace  RPG.Control
         const float waypointGizmosRadius = 0.25f;
         void OnDrawGizmos()
         {
-            for(int i = 0; i < transform.childCount; i++) 
+            for(int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmosRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmosRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int index)
+        {
+            return transform.GetChild(index).position;
         }
     }
 }
